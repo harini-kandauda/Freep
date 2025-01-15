ad8767eb995dee3b61a6daba4dd6cccc8979f945
Harini Kandauda <harini.kandauda@hotmail.fr>
2025-01-15 09:54:21 +0100

    linked create account vue to list article vue

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,24 +2,21 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import SignUp from '../views/SignUp.vue'
 import Success_SignUp from '../views/Success_SignUp.vue'
+import ArticleList from '/src/views/ArticleList.vue'
 
 const routes = [
     {
        path: '/',
-       redirect: '/home',
-    },
-    {
-       path: '/home',
-       component: Home,
+       redirect: '/articlelist',
     }, 
+    {
+      path: '/articlelist',
+      component: ArticleList,
+    },
     {
       path: '/signup',
       component: SignUp,
     },
-    {
-      path: '/success',
-      component: Success_SignUp,
-    }
 ]
 
 const router = createRouter({
--- frontend/src/views/Success_SignUp.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    Compte créé avec succès
-</template>
