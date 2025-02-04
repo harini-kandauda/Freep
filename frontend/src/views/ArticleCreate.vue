96a96c6a1f7e03ceaadcc5fe5bfe70b7a8e7ca5f
Harini Kandauda <harini.kandauda@hotmail.fr>
2025-02-04 15:23:37 +0100

    added form selects

--- frontend/src/views/ArticleCreate.vue
+++ frontend/src/views/ArticleCreate.vue
@@ -6,8 +6,53 @@
       <input type="text" name="article_title" v-model="formData.title" />
     </div>
     <div>
+      <label for="article_size">Type</label>
+      <select v-model="formData.type">
+        <option disabled value="">Veuillez sélectionner un type</option>
+        <option>Hauts</option>
+        <option>Bas</option>
+        <option>Accessoires</option>
+        <option>Vêtements de soirée</option>
+        <option>Déguisement</option>
+        <option>Vêtements de sport</option>
+      </select>
+    </div>
+    <div>
       <label for="article_desc">Description</label>
       <textarea name="article_desc" v-model="formData.desc" rows="5"></textarea>
+    </div>
+    <div>
+      <label for="article_size">Genre</label>
+      <select v-model="formData.genders">
+        <option disabled value="">Veuillez sélectionner un genre</option>
+        <option>Homme</option>
+        <option>Femme</option>
+        <option>Unisex</option>
+      </select>
+    </div>
+    <div>
+      <label for="article_size">Taille</label>
+      <select v-model="formData.size">
+        <option disabled value="">Veuillez sélectionner une taille</option>
+        <option>XS</option>
+        <option>S</option>
+        <option>M</option>
+        <option>L</option>
+        <option>XL</option>
+        <option>XXL</option>
+      </select>
+    </div>
+    <div>
+      <label for="article_size">Etat</label>
+      <select v-model="formData.state">
+        <option disabled value="">Veuillez sélectionner un état</option>
+        <option>Neuf</option>
+        <option>Comme Neuf</option>
+        <option>Très bon état</option>
+        <option>Bon état</option>
+        <option>Usé</option>
+        <option>Abimé</option>
+      </select>
     </div>
     <button type="submit" value="submit">Enregistrer</button>
   </form>
